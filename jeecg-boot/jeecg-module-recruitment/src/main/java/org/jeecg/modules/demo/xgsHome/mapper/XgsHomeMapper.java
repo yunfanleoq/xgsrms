14459bded9ac0a1ec73c74455c035b500837d0bd
ruochen <ruochenen@gmail.com>
2025-01-10 13:14:30 +0800

    首页

---
+++ jeecg-boot/jeecg-module-recruitment/src/main/java/org/jeecg/modules/demo/xgsHome/mapper/XgsHomeMapper.java
@@ -0,0 +1,17 @@
+package org.jeecg.modules.demo.xgsHome.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.jeecg.modules.demo.xgsHome.entity.XgsHome;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 首页
+ * @Author: jeecg-boot
+ * @Date:   2025-01-10
+ * @Version: V1.0
+ */
+public interface XgsHomeMapper extends BaseMapper<XgsHome> {
+
+}
